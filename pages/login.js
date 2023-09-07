6f5e873b05dfde06652df6acdd94dd8623e664e4
speedkjjh <speedkjjh@gmail.com>
2023-09-07 19:52:15 -0400

    Pages

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,68 @@
 import Link from 'next/link';
 import Navbar from '../components/Navbar';
+import { useState } from 'react';
+import axios from 'axios'; // Import the axios library to make HTTP requests
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Clear previous errors
+        setError(null);
+
+        try {
+            // Simulate an API call to authenticate the user
+            // Note: You'd need to have an actual API endpoint to handle this in a real application.
+            const response = await axios.post('/api/auth/login', { email, password });
+
+            // If successful, store the authentication token in local storage
+            const token = response.data.token;
+            localStorage.setItem('authToken', token);
+
+            // Redirect to the home page or dashboard after successful login
+            window.location.href = '/';
+
+        } catch (err) {
+            // Handle error responses from the server
+            setError(err.response?.data?.message || 'An error occurred during login');
+        }
+    };
+
     return (
         <div>
             <Navbar />
-            <h1>Login</h1>
-            {/* Add your login form here */}
+            <h1>Log In</h1>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <button type="submit">Log In</button>
+                </div>
+            </form>
             <p>Don't have an account? <Link href="/signup">Sign Up</Link></p>
         </div>
     );
-}
+};
 
 export default Login;
---
+++ pages/logout.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import Link from 'next/link';
+
+const Logout = () => {
+
+    useEffect(() => {
+        // Clear the user authentication token (or any other related user data).
+        // This assumes the token is stored in local storage.
+        localStorage.removeItem('authToken');
+
+        // Optionally, you can redirect users immediately after logging out.
+        // For instance, to redirect to the home page, use the following:
+        // router.push('/');
+
+    }, []);  // The empty dependency array ensures this effect runs once when the component mounts.
+
+    return (
+        <div>
+            <h1>You've been logged out</h1>
+            <p>
+                <Link href="/"><a>Return to Home Page</a></Link>
+            </p>
+        </div>
+    );
+};
+
+export default Logout;
