d44b2e11f776922eeaf34367f63694209783a635
speedkjjh <speedkjjh@gmail.com>
2023-09-08 09:35:40 -0400

    backend integration

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -3,21 +3,21 @@ import { useDebounce } from 'use-debounce';
 import Form from 'react-bootstrap/Form';
 
 const SearchBar = ({ handleSearch }) => {
-    const [text, setText] = useState('');
-    const [debouncedText] = useDebounce(text, 500);
+  const [text, setText] = useState('');
+  const [debouncedText] = useDebounce(text, 500);
 
-    useEffect(() => {
-        handleSearch(debouncedText);
-    }, [debouncedText, handleSearch]);
+  useEffect(() => {
+    handleSearch(debouncedText);
+  }, [debouncedText, handleSearch]);
 
-    return (
-        <Form.Control
-            type="text"
-            placeholder="Search by username..."
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-        />
-    );
+  return (
+    <Form.Control
+      type="text"
+      placeholder="Search by username..."
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+    />
+  );
 };
 
 export default SearchBar;
--- components/SignUp.js
+++ components/SignUp.js
@@ -4,39 +4,39 @@ import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 
 function SignUp() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const response = await axios.post('/api/signup', { username, email, password });
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/api/signup', { username, email, password });
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="formGroupUsername">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formGroupPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Sign Up
-            </Button>
-        </Form>
-    );
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formGroupUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Sign Up
+      </Button>
+    </Form>
+  );
 }
 
 export default SignUp;
