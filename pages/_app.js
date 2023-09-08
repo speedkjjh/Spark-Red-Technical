3d3c1d9533295b522c92b86383c9dcfc09ef431b
speedkjjh <speedkjjh@gmail.com>
2023-09-08 07:31:35 -0400

    backend integration

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '@styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default Application
