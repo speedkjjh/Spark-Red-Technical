f492e70e8ffe0751efe7487f48f5f50f1b09937a
speedkjjh <speedkjjh@gmail.com>
2023-09-08 03:43:36 -0400

    Pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,35 @@
-import Link from 'next/link';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const Navbar = () => {
+const BasicNavbar = () => {
     return (
-        <nav>
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/login"><a>Log In</a></Link>
-            <Link href="/signup"><a>Sign Up</a></Link>
-        </nav>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="/">Y</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/">Login</Nav.Link>  {/* Duplicate link? */}
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
-};
+}
 
-export default Navbar;
+export default BasicNavbar;
