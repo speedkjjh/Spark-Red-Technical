515d979eeb115171b0bc15429a97ad27ab9f1dc7
speedkjjh <speedkjjh@gmail.com>
2023-09-08 03:47:30 -0400

    Pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,29 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// Importing necessary components from react-bootstrap
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 const BasicNavbar = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
+                {/* Brand Logo */}
                 <Navbar.Brand href="/">Y</Navbar.Brand>
+
+                {/* Toggle button for smaller screens */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                {/* Navbar Items */}
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/login">Login</Nav.Link>
-                        <Nav.Link href="/">Login</Nav.Link>  {/* Duplicate link? */}
+
+                        {/* Dropdown Menu */}
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
