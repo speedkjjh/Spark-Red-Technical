49a798d9321cf519995f2fc5a5c2b42dde7f8a6b
speedkjjh <speedkjjh@gmail.com>
2023-09-08 03:57:56 -0400

    Pages

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const tweetRoutes = require('./lib/routes/tweetRoutes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://speedkjjh:Legitpassword123@jjtechnicaldb.beqquzi.mongodb.net/?retryWrites=true&w=majority', {
+// Use environment variables for sensitive information
+const MONGO_URI = process.env.MONGO_URI || 'your_default_mongo_uri_here';
+
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // if you need to handle form submissions
 
 // User Authentication Endpoints
 app.use('/api/auth', authRoutes);
@@ -20,7 +22,7 @@ app.use('/api/auth', authRoutes);
 // Tweet-related Endpoints
 app.use('/api/tweets', tweetRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
