7d9242834c3c2abec2bf84bfe656f1a4717f1930
speedkjjh <speedkjjh@gmail.com>
2023-09-08 11:35:30 -0400

    backend integration

--- components/AnonNavbar.js
+++ components/AnonNavbar.js
@@ -6,11 +6,9 @@ const AnonNavbar = () => {
         <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
             <Container>
                 <Navbar.Brand href="/">Y-witter</Navbar.Brand>
-
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Nav className="me-auto my-2 my-lg-0"
-                         style={{ maxHeight: '100px' }}
-                         navbarScroll>
+                         style={{ maxHeight: '100px' }}>
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/login">Login</Nav.Link>
                         <Nav.Link href="/signup">Signup</Nav.Link>
